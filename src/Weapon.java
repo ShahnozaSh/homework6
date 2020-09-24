6322e8335c1bfa57b609705ac201706ae44e6efc
ShahnozaSh <shahnoza_3010@mai.ru>
2020-09-24 18:03:13 +0600

    Initial commit

---
+++ src/Weapon.java
@@ -0,0 +1,14 @@
+public class Weapon {
+    private String typeofWeapon;
+    private String nameofWeapon;
+
+ public String getTypeofWeapon(){
+ return  typeofWeapon;}
+ public String getNameofWeapon(){
+     return nameofWeapon;
+ }
+ public Weapon(String typeofWeapon, String nameofWeapon){
+     this.typeofWeapon= typeofWeapon;
+     this.nameofWeapon= nameofWeapon;
+ }
+}
